c7bc014c7fb7fd8ebcb04b4a951b90dfdd104682
niceman114 <niceman114@gmail.com>
2023-07-13 12:09:57 +0900

    Init

---
+++ src/main/java/how/to/jacoco/JacocoApplication.java
@@ -0,0 +1,13 @@
+package how.to.jacoco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacocoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JacocoApplication.class, args);
+    }
+
+}
